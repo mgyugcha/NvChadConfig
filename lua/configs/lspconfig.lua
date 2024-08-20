3850114abbb1e24a8d5293d2b7b4ee69ced2a344
Michael Yugcha <mgyugcha@gmail.com>
2024-08-20 10:53:27 -0500

    Configuración inicial de nvim
    - Se configuró prettier para archivos de typescript
    - Se configuró el servidor de lsp para typescript y la versión de
      typescript con react
    - Se configuró lsp para tailwind

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver", "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -18,7 +18,7 @@ end
 
 -- configuring single server, example: typescript
 -- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
+--  on_attach = nvlsp.on_attach,
+--  on_init = nvlsp.on_init,
+--  capabilities = nvlsp.capabilities,
 -- }
